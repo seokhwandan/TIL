b6fc72ef1c3e967c42058854418a5352abbe9ba4
seokhwandan <dansh87@gmail.com>
2021-04-06 00:20:15 +0900

    swea 1232_사칙연산

--- algorithm/0405/1232_사칙연산.py
+++ algorithm/0405/1232_사칙연산.py
@@ -19,13 +19,13 @@ def postorder(node):
 
 for t in range(1, 11):
     V = int(input())
-    tree = [[0] * 3 for _ in range(V + 1)]
+    tree = [[0] * 3 for _ in range(V + 1)] # 0: 부모 1, 2: 자식
     for i in range(1, V + 1):
         temp = input().split()
-        if len(temp) > 2:
-            tree[i][0] = temp[1]
+        if len(temp) > 2: # 부모가 부호이고, 두개의 자식노드가 있을 때
+            tree[i][0] = temp[1] 
             tree[i][1] = int(temp[2])
             tree[i][2] = int(temp[3])
-        else:
+        else: # temp 의 길이가 2 즉, 해당 노드의 value
             tree[i][0] = int(temp[1])
     print('#{} {}'.format(t, postorder(1)))
